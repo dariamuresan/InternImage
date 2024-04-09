0b911a63ed2d4b96ca0d6ce9362cdd041a29ec3b
Daria <muresan.daria14@yahoo.com>
2024-04-09 21:33:29 +0000

    Reproduced results

--- segmentation/train.py
+++ segmentation/train.py
@@ -30,7 +30,7 @@ import mmseg_custom  # noqa: F401,F403
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a segmentor')
-    parser.add_argument('config', help='train config file path')
+    parser.add_argument('--config', help='train config file path')
     parser.add_argument('--work-dir', help='the dir to save logs and models')
     parser.add_argument('--load-from',
                         help='the checkpoint file to load weights from')
